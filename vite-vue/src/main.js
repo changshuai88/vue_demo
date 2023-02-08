0f85198bc8892c751f7bf904a369234317ad980a
Bob Chang <changxuanyuan@sina.com>
2023-02-08 10:29:41 +0800

    中途添加vuex

--- vite-vue/src/main.js
+++ vite-vue/src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
+
 // import ElementUI from 'element-ui';
 import 'element-plus/dist/index.css'
 
+import store from './store'
 
 
-createApp(App).use(router).use(ElementPlus).mount('#app')
+
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
