b382a45ee4ee7c2ba4dc9365ddafc8a133592069
Bob Chang <changxuanyuan@sina.com>
2023-02-09 16:32:16 +0800

    获取url信息

--- vuex-demo4/src/views/HomeView.vue
+++ vuex-demo4/src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="getStore()">获取路由</button>
   </div>
 </template>
 
@@ -13,6 +14,15 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  methods: {
+    getStore () {
+      console.log(this.$route)
+    }
+  },
+  mounted () {
+    console.log(this.$route)
   }
+
 }
 </script>
