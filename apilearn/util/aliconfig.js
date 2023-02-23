e5b5503a357abeda3b8885eda2d83edf34fb7553
Bob Chang <2233282817@qq.com>
2023-02-23 14:10:34 +0800

    测试忽略上传

--- apilearn/util/aliconfig.js
+++ apilearn/util/aliconfig.js
@@ -1,7 +1,7 @@
 module.exports={
     alicloud:{
-        'accessKeyId': '<your-access-key-id>',
-        'accessKeySecret': '<your-access-key-secret>',
+        'accessKeyId': '12324',
+        'accessKeySecret': '1234442',
         // securityToken: '<your-sts-token>', // use STS Token
         'endpoint': 'https://dysmsapi.aliyuncs.com',
         'apiVersion': '2017-05-25' 
